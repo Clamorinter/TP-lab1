ef39eca8625593f676760c7e492482838f64fba4
Clamorinter <MRakhilov@gmail.com>
2023-10-03 20:09:52 +0300

    Added book header file

---
+++ book.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "bookshop.h"
+#include <string>
+
+class Book : public Bookshop
+{
+public:
+	Book();
+//	Book(); with param
+	Book(const Book& original);
+	
+	void fstream_in(const std::ifstream& fin);
+	Bookshop* fstream_out(const std::ofstream& fout);
+
+	const std::string get_name();
+	const std::string get_author();
+	const int get_year();
+	const std::string get_annotation();
+	const std::string get_genre();
+	const int get_num_of_pages();
+	const double get_price();
+
+	void set_name(std::string name);
+	void set_author(std::string author);
+	void set_year(int year);
+	void set_annotation(std::string annotation);
+	void set_genre(std::string genre);
+	void set_num_of_pages(int num_of_pages);
+	void set_price(double price);
+
+private:
+	std::string name;
+	std::string author;
+	int year;
+	std::string annotation;
+	std::string genre;
+	int num_of_pages;
+	double price;
+};
--- bookshop.h
+++ bookshop.h
@@ -6,9 +6,13 @@ class Bookshop
 {
 public:
 	Bookshop();
-	
+	virtual ~Bookshop();
+
 	virtual void fstream_in(const std::ifstream &fin) = 0;
 	virtual Bookshop* fstream_out(const std::ofstream& fout) = 0;
+
+	const char get_type();
+	void set_type(char type);
 private:
 	char type;
 };
